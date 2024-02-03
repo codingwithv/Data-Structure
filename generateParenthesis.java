2b21b1a07de7a3e0dbf24e09667787225a178b76
codingwithv <vivekkumardilliya@gmail.com>
2024-02-03 13:43:42 +0530

    added README.md

---
+++ generateParenthesis.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    public void solve(List<String> result, int left, int right, String s, int n){
+        if(s.length() == n*2){
+            result.add(s);
+            return;
+        }
+
+        if(left < n){
+            solve(result, left+1, right, s+"(", n);
+        }
+
+        if(right < left){
+            solve(result, left, right+1, s+")", n);
+        }
+    }
+
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<String>();
+        solve(result,0,0,"",n);
+        return result;
+    }
+}
