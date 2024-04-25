035dfb76e6dba3db8f50ecca349ecb627e3a5a9e
vivek singh <102433383+codingwithv@users.noreply.github.com>
2024-04-26 00:14:06 +0530

    Leetcode : 2131

---
+++ Greedy/Longest Palindrome by Concatenating Two Letter Words.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestPalindrome(String[] words) {
+        int counter[][] = new int[26][26];
+        int ans = 0;
+        for(String s : words){
+            int a = s.charAt(0) - 'a';
+            int b = s.charAt(1) - 'a';
+            if(counter[b][a] > 0){
+                ans += 4;
+                counter[b][a]--;
+            }
+            else{
+                counter[a][b]++;
+            }
+        }
+        for(int i = 0; i < 26; i++){
+            if(counter[i][i] > 0){
+                ans += 2;
+                break;
+            }
+        }
+        return ans;
+    }
+}
