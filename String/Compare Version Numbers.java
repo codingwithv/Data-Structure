f56a89c6117976c5f9f89a356142fdbd3558f5f7
vivek singh <102433383+codingwithv@users.noreply.github.com>
2024-05-03 23:34:27 +0530

    Leetcode: 165

---
+++ String/Compare Version Numbers.java
@@ -0,0 +1,39 @@
+// Using StringTokenizer
+
+import java.util.StringTokenizer;
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        List<String> v1 = getTokens(version1);
+        List<String> v2 = getTokens(version2);
+
+        int m = v1.size();
+        int n = v2.size();
+
+        int i = 0;
+
+        while(i < m || i < n){
+            int a = i < m ? Integer.parseInt(v1.get(i)) : 0;
+            int b = i < n ? Integer.parseInt(v2.get(i)) : 0;
+
+            if(a > b){
+                return 1;
+            }
+            else if(b > a){
+                return -1;
+            }
+            else{
+                i++;
+            }
+        }
+        return 0;
+    }
+
+    private List<String> getTokens(String version){
+        StringTokenizer token = new StringTokenizer(version, ".");
+        List<String> versionTokens = new ArrayList<>();
+        while(token.hasMoreTokens()){
+            versionTokens.add(token.nextToken());
+        }
+        return versionTokens;
+    }
+}
